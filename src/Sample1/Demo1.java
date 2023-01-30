01a52b060973565e7b9d0d1f79879043c6433531
rasho <rasho@HP>
2023-01-30 13:37:37 +0530

    [Ashok]: 1st changes in project

---
+++ src/Sample1/Demo1.java
@@ -0,0 +1,11 @@
+package Sample1;
+
+public class Demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Good Morning");
+		System.out.println("hello");
+	}
+
+}   //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 
